9f0c2aabddaf782d3ec79aace54323c34f3a1556
altaf <altafallahabbassi@gmail.com>
2022-07-29 11:50:08 -0400

    add req.txt for compute canada servers

--- Examples/simpletod/train_agent_cc.sh
+++ Examples/simpletod/train_agent_cc.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #SBATCH -t 2-12:00
-#SBATCH --mem=5000 
+#SBATCH --mem=5000
+#SBATCH --cpus-per-task=24 
+
+
+module load python/3.7
+virtualenv --no-download ENV
+source ENV/bin/activate
+
+pip install --no-index -r requirements_cc.txt
+
+pip install --no-index -e .
+
 python Examples/simpletod/train_agent.py \
  --top-k 3 \
  --evaluation_episodes 0  \
